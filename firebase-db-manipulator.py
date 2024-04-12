e19bdbf9deceb766e9ff3bd931cc7e85d4064923
RaduBratan <bratanradu7@gmail.com>
2024-04-12 17:46:43 +0300

    Add developer and categories, prevent some fields from being udpated in certain cases

--- firebase-db-manipulator.py
+++ firebase-db-manipulator.py
@@ -51,15 +51,25 @@ def add_game_from_input():
     game_data["searchName"] = re.sub(
         r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
     )
+    game_data["developer"] = input("Enter the developer: ")
     game_data["icon"] = input("Enter the icon URL: ")
     game_data["banner"] = input("Enter the banner URL: ")
     game_data["description"] = input("Enter the description: ")
     game_data["price"] = float(input("Enter the price: "))
     game_data["genre"] = float(input("Enter the genre: "))
+
+    categories_input = input("Enter the categories (comma-separated, without space): ")
+    game_data["categories"] = (
+        [category.strip() for category in categories_input.split(",")]
+        if categories_input
+        else []
+    )
+
     tags_input = input("Enter the tags (comma-separated, without space): ")
     game_data["tags"] = (
         [int(tag) for tag in tags_input.split(",")] if tags_input else []
     )
+
     game_data["dateAdded"] = firestore.SERVER_TIMESTAMP
 
     db.collection("games").document(game_data["id"]).set(game_data)
@@ -71,7 +81,9 @@ def add_game_from_scraper():
     game_data = {}
     game_data["id"] = db.collection("games").document().id
     game_data["packageName"] = input("Enter the package name: ")
+
     result = app(game_data["packageName"], country="us", lang="en")
+
     game_data["displayName"] = result["title"]
     game_data["displayName"] = (
         game_data["displayName"].replace("™", "").replace("©", "").replace("®", "")
@@ -79,18 +91,24 @@ def add_game_from_scraper():
     game_data["searchName"] = re.sub(
         r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
     )
+    game_data["developer"] = result["developer"]
     game_data["icon"] = result["icon"]
+
     if input("Would you like to scrape the banner? (y/n): ").lower() == "y":
         game_data["banner"] = result["headerImage"]
     else:
         game_data["banner"] = input("Enter the banner URL: ")
+
     game_data["description"] = result["summary"]
     game_data["price"] = float(result["price"])
     game_data["genre"] = result["genre"]
+    game_data["categories"] = [category["name"] for category in result["categories"]]
+
     tags_input = input("Enter the tags (comma-separated, without space): ")
     game_data["tags"] = (
         [int(tag) for tag in tags_input.split(",")] if tags_input else []
     )
+
     game_data["dateAdded"] = firestore.SERVER_TIMESTAMP
 
     db.collection("games").document(game_data["id"]).set(game_data)
@@ -102,6 +120,7 @@ def update_game_from_input(package_name):
     doc_ref = (
         db.collection("games").where("packageName", "==", package_name).limit(1).get()
     )
+
     if not doc_ref:
         print(f"No game found with packageName: {package_name}")
         return
@@ -114,17 +133,6 @@ def update_game_from_input(package_name):
     if input("Would you like to update the packageName? (y/n): ").lower() == "y":
         game_data["packageName"] = input("Enter the new packageName: ")
 
-    if input("Would you like to update the banner URL? (y/n): ").lower() == "y":
-        game_data["banner"] = input("Enter the new banner URL: ")
-
-    if input("Would you like to update the tags? (y/n): ").lower() == "y":
-        game_data["tags"] = [
-            int(tag)
-            for tag in input(
-                "Enter the new tags (comma-separated, without space): "
-            ).split(",")
-        ]
-
     if input("Would you like to update the name? (y/n): ").lower() == "y":
         game_data["displayName"] = input("Enter the new name: ")
 
@@ -135,8 +143,14 @@ def update_game_from_input(package_name):
         r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
     )
 
+    if input("Would you like to update the developer? (y/n): ").lower() == "y":
+        game_data["developer"] = input("Enter the new developer: ")
+
     if input("Would you like to update the icon URL? (y/n): ").lower() == "y":
         game_data["icon"] = input("Enter the new icon URL: ")
+
+    if input("Would you like to update the banner URL? (y/n): ").lower() == "y":
+        game_data["banner"] = input("Enter the new banner URL: ")
 
     if input("Would you like to update the description? (y/n): ").lower() == "y":
         game_data["description"] = input("Enter the new description: ")
@@ -147,30 +161,13 @@ def update_game_from_input(package_name):
     if input("Would you like to update the genre? (y/n): ").lower() == "y":
         game_data["genre"] = float(input("Enter the new genre: "))
 
-    db.collection("games").document(doc_ref[0].id).update(game_data)
-    print("Game updated successfully")
-
-
-# ---------------- 4. UPDATE A GAME FROM SCRAPER AND INPUT ----------------
-def update_game_from_scraper(package_name):
-    doc_ref = (
-        db.collection("games").where("packageName", "==", package_name).limit(1).get()
-    )
-    if not doc_ref:
-        print(f"No game found with packageName: {package_name}")
-        return
-
-    game_data = doc_ref[0].to_dict()
-
-    print(f"Current data for game with packageName: {package_name}")
-    print(json.dumps(game_data, indent=2, ensure_ascii=False, cls=CustomJSONEncoder))
-
-    if input("Would you like to update the banner URL? (y/n): ").lower() == "y":
-        if game_data["banner"].startswith("https://play-lh.googleusercontent.com"):
-            result = app(package_name, country="us", lang="en")
-            game_data["banner"] = result["headerImage"]
-        else:
-            game_data["banner"] = input("Enter the new banner URL: ")
+    if input("Would you like to update the categories? (y/n): ").lower() == "y":
+        game_data["categories"] = [
+            category.strip()
+            for category in input(
+                "Enter the new categories (comma-separated, without space): "
+            ).split(",")
+        ]
 
     if input("Would you like to update the tags? (y/n): ").lower() == "y":
         game_data["tags"] = [
@@ -180,49 +177,67 @@ def update_game_from_scraper(package_name):
             ).split(",")
         ]
 
-    if input("Would you like to update the packageName? (y/n): ").lower() == "y":
-        package_name_input = input("Enter the new packageName: ")
-        result = app(package_name_input, country="us", lang="en")
-        game_data["packageName"] = package_name_input
-        package_name = package_name_input
-        game_data["displayName"] = result["title"]
-        game_data["displayName"] = (
-            game_data["displayName"].replace("™", "").replace("©", "").replace("®", "")
-        )
-        game_data["searchName"] = re.sub(
-            r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
-        )
+    db.collection("games").document(doc_ref[0].id).update(game_data)
+    print("Game updated successfully")
+
+
+# ---------------- 4. UPDATE A GAME FROM SCRAPER AND INPUT ----------------
+def update_game_from_scraper(package_name):
+    doc_ref = (
+        db.collection("games").where("packageName", "==", package_name).limit(1).get()
+    )
+
+    if not doc_ref:
+        print(f"No game found with packageName: {package_name}")
+        return
+
+    game_data = doc_ref[0].to_dict()
+    result = app(package_name, country="us", lang="en")
+
+    print(f"Current data for game with packageName: {package_name}")
+    print(json.dumps(game_data, indent=2, ensure_ascii=False, cls=CustomJSONEncoder))
+
+    if input("Would you like to update everything at once? (y/n): ").lower() == "y":
+        game_data["developer"] = result["developer"]
         game_data["icon"] = result["icon"]
+        game_data["banner"] = result["headerImage"]
         game_data["description"] = result["summary"]
         game_data["price"] = float(result["price"])
         game_data["genre"] = result["genre"]
+        game_data["categories"] = [
+            category["name"] for category in result["categories"]
+        ]
     else:
-        if input("Would you like to update the name? (y/n): ").lower() == "y":
-            result = app(package_name, country="us", lang="en")
-            game_data["displayName"] = result["title"]
-
-        game_data["displayName"] = (
-            game_data["displayName"].replace("™", "").replace("©", "").replace("®", "")
-        )
-        game_data["searchName"] = re.sub(
-            r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
-        )
+        if input("Would you like to update the developer? (y/n): ").lower() == "y":
+            game_data["developer"] = result["developer"]
 
         if input("Would you like to update the icon URL? (y/n): ").lower() == "y":
-            result = app(package_name, country="us", lang="en")
             game_data["icon"] = result["icon"]
 
+        if input("Would you like to update the banner URL? (y/n): ").lower() == "y":
+            game_data["banner"] = result["headerImage"]
+
         if input("Would you like to update the description? (y/n): ").lower() == "y":
-            result = app(package_name, country="us", lang="en")
             game_data["description"] = result["summary"]
 
         if input("Would you like to update the price? (y/n): ").lower() == "y":
-            result = app(package_name, country="us", lang="en")
             game_data["price"] = float(result["price"])
 
         if input("Would you like to update the genre? (y/n): ").lower() == "y":
-            result = app(package_name, country="us", lang="en")
             game_data["genre"] = result["genre"]
+
+        if input("Would you like to update the categories? (y/n): ").lower() == "y":
+            game_data["categories"] = [
+                category["name"] for category in result["categories"]
+            ]
+
+    if input("Would you like to update the tags? (y/n): ").lower() == "y":
+        game_data["tags"] = [
+            int(tag)
+            for tag in input(
+                "Enter the new tags (comma-separated, without space): "
+            ).split(",")
+        ]
 
     db.collection("games").document(doc_ref[0].id).update(game_data)
     print("Game updated successfully")
@@ -244,17 +259,15 @@ def update_games_from_scraper():
 
         if game_data["banner"].startswith("https://play-lh.googleusercontent.com"):
             game_data["banner"] = result["headerImage"]
-        # game_data["displayName"] = result["title"]
-        # game_data["displayName"] = (
-        #     game_data["displayName"].replace("™", "").replace("©", "").replace("®", "")
-        # )
-        # game_data["searchName"] = re.sub(
-        #     r"[^a-z0-9]", "", unidecode.unidecode(game_data["displayName"].lower())
-        # )
+
+        game_data["developer"] = result["developer"]
         game_data["icon"] = result["icon"]
         game_data["description"] = result["summary"]
         game_data["price"] = float(result["price"])
         game_data["genre"] = result["genre"]
+        game_data["categories"] = [
+            category["name"] for category in result["categories"]
+        ]
 
         db.collection("games").document(doc.id).update(game_data)
         print(f"Updated game with packageName: {package_name}")
@@ -277,17 +290,6 @@ def delete_games():
             print(f"Deleted game with packageName: {package_name}")
 
     print("Games deleted successfully")
-
-
-# ---------------- ?. SHOW ALL GAMES ----------------
-def list_games():
-    docs = db.collection("games").stream()
-
-    for doc in docs:
-        game = doc.to_dict()
-        print(f"Name: {game['name']}, PackageName: {game['packageName']}")
-
-    print("Games listed successfully")
 
 
 # ---------------- 7. SEARCH FOR A GAME ----------------
@@ -325,6 +327,17 @@ def search_game():
     print(json.dumps(apps_details, indent=2, ensure_ascii=False))
 
 
+# ---------------- ?. SHOW ALL GAMES ----------------
+def list_games():
+    docs = db.collection("games").stream()
+
+    for doc in docs:
+        game = doc.to_dict()
+        print(f"Name: {game['name']}, PackageName: {game['packageName']}")
+
+    print("Games listed successfully")
+
+
 # ---------------- MAIN ----------------
 def main():
     while True:
@@ -333,10 +346,10 @@ def main():
         print("2. Add a game using scraper")
         print("3. Update a game using console")
         print("4. Update a game using scraper")
-        print("5. Update all games using scraper")  # !!! WARNING: COSTLY
+        print("5. Update all games using scraper (costly)")
         print("6. Delete certain games")
-        # print("?. List all games")  # !!! !!! WARNING: SUPER COSTLY
         print("7. Search for a game")
+        # print("?. List all games (super costly)")
         print("8. Exit")
 
         option = input("Select an option: ")
@@ -354,10 +367,10 @@ def main():
             update_games_from_scraper()
         elif option == "6":
             delete_games()
-        # elif option == "?":
-        #     list_games()
         elif option == "7":
             search_game()
+        # elif option == "?":
+        #     list_games()
         elif option == "8":
             break
         else:
